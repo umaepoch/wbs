2b24ef26d1ad288848589ebbefaad3a21ffa1bcd
NightsOfCoding <yashgowdaan@gmail.com>
2022-01-31 16:24:58 +0530

    redirect to wbs settings.

--- wbs/custom/warehouse/warehouse.js
+++ wbs/custom/warehouse/warehouse.js
@@ -3,8 +3,9 @@ frappe.ui.form.on('Warehouse', {
 
     if (!frm.doc.__islocal) {
       frm.add_custom_button(__('Make WBS Warehouse & Specify WBS Settings'),() => {
-        let new_doc = frappe.model.get_new_doc('WBS Settings');
-        frappe.set_route('Form/WBS Settings/New WBS Settings', {'warehouse': frm.doc.name});
+        let local_docname = frappe.model.make_new_doc_and_get_name('WBS Settings');
+        // let new_doc = frappe.new_doc('WBS Settings');
+        frappe.set_route('Form/WBS Settings',local_docname, {'warehouse': frm.doc.name});
       });
 
     }
