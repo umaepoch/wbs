e6c09622393a6e3efb60ae53519aeef351856a5c
NightsOfCoding <yashgowdaan@gmail.com>
2022-01-31 16:48:07 +0530

    Redirect testing.

--- wbs/custom/warehouse/warehouse.js
+++ wbs/custom/warehouse/warehouse.js
@@ -5,7 +5,8 @@ frappe.ui.form.on('Warehouse', {
       frm.add_custom_button(__('Make WBS Warehouse & Specify WBS Settings'),() => {
         let local_docname = frappe.model.make_new_doc_and_get_name('WBS Settings');
         // let new_doc = frappe.new_doc('WBS Settings');
-        frappe.set_route('Form/WBS Settings',local_docname, {'warehouse': frm.doc.name});
+        console.log(frappe.get_route())
+        // frappe.set_route('Form/WBS Settings',local_docname, {'warehouse': frm.doc.name});
       });
 
     }
