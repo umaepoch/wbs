799038d2cc9fe49f10bb2a7ac2dedd0dfbf5b298
NightsOfCoding <yashgowdaan@gmail.com>
2022-02-08 18:40:09 +0530

    wbs stock ledger report.

--- wbs/wbs/report/wbs_stock_ledger_report/wbs_stock_ledger_report.py
+++ wbs/wbs/report/wbs_stock_ledger_report/wbs_stock_ledger_report.py
@@ -9,13 +9,95 @@ from erpnext.stock.utils import update_included_uom_in_report
 from erpnext.stock.doctype.serial_no.serial_no import get_serial_nos
 
 def execute(filters=None):
-	data = []
+	include_uom = filters.get("include_uom")
 	columns = get_columns()
 	items = get_items(filters)
 	sl_entries = get_stock_ledger_entries(filters, items)
+	item_details = get_item_details(items, sl_entries, include_uom)
+	opening_row = get_opening_balance(filters, columns)
+	precision = cint(frappe.db.get_single_value("System Settings", "float_precision"))
 
+	data = []
+	conversion_factors = []
+	if opening_row:
+		data.append(opening_row)
+
+	actual_qty = stock_value = 0
+
+	available_serial_nos = {}
+	for sle in sl_entries:
+
+		item_detail = item_details[sle.item_code]
+		sle.update(item_detail)
+
+		if filters.get("batch_no"):
+			actual_qty += flt(sle.actual_qty, precision)
+			stock_value += sle.stock_value_difference
+
+			if sle.voucher_type == 'Stock Reconciliation' and not sle.actual_qty:
+				actual_qty = sle.qty_after_transaction
+				stock_value = sle.stock_value
+
+			sle.update({
+				"qty_after_transaction": actual_qty,
+				"stock_value": stock_value
+			})
+
+		if sle.serial_no:
+			update_available_serial_nos(available_serial_nos, sle)
+
+		data.append(sle)
+
+		if include_uom:
+			conversion_factors.append(item_detail.conversion_factor)
+
+	update_included_uom_in_report(columns, data, include_uom, conversion_factors)
+
+	if filters.get('wbs_storage_location'):
+		data = update_wbs_storage_location(data, filters)
 	return columns, data
 
+def update_wbs_storage_location(data, filters):
+	if data:
+		for d in data:
+			if d.voucher_no:
+				wbs_conditions_sql = "select source_warehouse_storage_location, target_warehouse_storage_location from `tabStock Entry Detail` where parent = %s"
+				if filters.get('wbs_storage_location'):
+					wbs_conditions_sql += "and (source_warehouse_storage_location = %s or target_warehouse_storage_location = %s)"
+
+				if wbs_conditions_sql:
+					location = frappe.db.sql(wbs_conditions_sql, (d.voucher_no, filters.get('wbs_storage_location'), filters.get('wbs_storage_location')), as_dict = 1)
+
+					if location:
+						print("UPDATING DICTIONARY")
+						d.update({
+							"source_warehouse_storage_location": location[len(location) - len(location)].source_warehouse_storage_location if location[len(location) - len(location)].source_warehouse_storage_location else "",
+							"target_warehouse_storage_location": location[len(location) - len(location)].target_warehouse_storage_location if location[len(location) - len(location)].target_warehouse_storage_location else ""
+						})
+
+	print(data)
+	return data
+
+def update_available_serial_nos(available_serial_nos, sle):
+	serial_nos = get_serial_nos(sle.serial_no)
+	key = (sle.item_code, sle.warehouse)
+	if key not in available_serial_nos:
+		available_serial_nos.setdefault(key, [])
+
+	existing_serial_no = available_serial_nos[key]
+	for sn in serial_nos:
+		if sle.actual_qty > 0:
+			if sn in existing_serial_no:
+				existing_serial_no.remove(sn)
+			else:
+				existing_serial_no.append(sn)
+		else:
+			if sn in existing_serial_no:
+				existing_serial_no.remove(sn)
+			else:
+				existing_serial_no.append(sn)
+
+	sle.balance_serial_no = '\n'.join(existing_serial_no)
 
 def get_columns():
 	columns = [
@@ -23,10 +105,11 @@ def get_columns():
 		{"label": _("Item"), "fieldname": "item_code", "fieldtype": "Link", "options": "Item", "width": 130},
 		{"label": _("Item Name"), "fieldname": "item_name", "width": 100},
 		{"label": _("Item Group"), "fieldname": "item_group", "fieldtype": "Link", "options": "Item Group", "width": 100},
+		{"label": _("Source Warehouse Storage Location"), "fieldname": "source_warehouse_storage_location", "fieldtype": "Link", "options": "WBS Storage Location", "width": 100},
+		{"label": _("Target Warehouse Storage Location"), "fieldname": "target_warehouse_storage_location", "fieldtype": "Link", "options": "WBS Storage Location", "width": 100},
 		{"label": _("Brand"), "fieldname": "brand", "fieldtype": "Link", "options": "Brand", "width": 100},
 		{"label": _("Description"), "fieldname": "description", "width": 200},
 		{"label": _("Warehouse"), "fieldname": "warehouse", "fieldtype": "Link", "options": "Warehouse", "width": 100},
-		{"label": _("WBS Storage Location"), "fieldname": "wbs_storage_location", "fieldtype": "Link", "options": "WBS Storage Location", "width": 100},
 		{"label": _("Stock UOM"), "fieldname": "stock_uom", "fieldtype": "Link", "options": "UOM", "width": 100},
 		{"label": _("Qty"), "fieldname": "actual_qty", "fieldtype": "Float", "width": 50, "convertible": "qty"},
 		{"label": _("Balance Qty"), "fieldname": "qty_after_transaction", "fieldtype": "Float", "width": 100, "convertible": "qty"},
@@ -48,22 +131,6 @@ def get_columns():
 	return columns
 
 
-def get_items(filters):
-	conditions = []
-	if filters.get("item_code"):
-		conditions.append("item.name=%(item_code)s")
-	else:
-		if filters.get("brand"):
-			conditions.append("item.brand=%(brand)s")
-		if filters.get("item_group"):
-			conditions.append(get_item_group_condition(filters.get("item_group")))
-
-	items = []
-	if conditions:
-		items = frappe.db.sql_list("""select name from `tabItem` item where {}"""
-			.format(" and ".join(conditions)), filters)
-	return items
-
 def get_stock_ledger_entries(filters, items):
 	item_conditions_sql = ''
 	if items:
@@ -83,3 +150,99 @@ def get_stock_ledger_entries(filters, items):
 			sle_conditions=get_sle_conditions(filters),
 			item_conditions_sql = item_conditions_sql
 		), filters, as_dict=1)
+
+def get_items(filters):
+	conditions = []
+	if filters.get("item_code"):
+		conditions.append("item.name=%(item_code)s")
+	else:
+		if filters.get("brand"):
+			conditions.append("item.brand=%(brand)s")
+		if filters.get("item_group"):
+			conditions.append(get_item_group_condition(filters.get("item_group")))
+
+	items = []
+	if conditions:
+		items = frappe.db.sql_list("""select name from `tabItem` item where {}"""
+			.format(" and ".join(conditions)), filters)
+	return items
+
+def get_item_details(items, sl_entries, include_uom):
+	item_details = {}
+	if not items:
+		items = list(set([d.item_code for d in sl_entries]))
+
+	if not items:
+		return item_details
+
+	cf_field = cf_join = ""
+	if include_uom:
+		cf_field = ", ucd.conversion_factor"
+		cf_join = "left join `tabUOM Conversion Detail` ucd on ucd.parent=item.name and ucd.uom=%s" \
+			% frappe.db.escape(include_uom)
+
+	res = frappe.db.sql("""
+		select
+			item.name, item.item_name, item.description, item.item_group, item.brand, item.stock_uom {cf_field}
+		from
+			`tabItem` item
+			{cf_join}
+		where
+			item.name in ({item_codes})
+	""".format(cf_field=cf_field, cf_join=cf_join, item_codes=','.join(['%s'] *len(items))), items, as_dict=1)
+
+	for item in res:
+		item_details.setdefault(item.name, item)
+
+	return item_details
+
+def get_sle_conditions(filters):
+	conditions = []
+	if filters.get("warehouse"):
+		warehouse_condition = get_warehouse_condition(filters.get("warehouse"))
+		if warehouse_condition:
+			conditions.append(warehouse_condition)
+	if filters.get("voucher_no"):
+		conditions.append("voucher_no=%(voucher_no)s")
+	if filters.get("batch_no"):
+		conditions.append("batch_no=%(batch_no)s")
+	if filters.get("project"):
+		conditions.append("project=%(project)s")
+
+	return "and {}".format(" and ".join(conditions)) if conditions else ""
+
+def get_opening_balance(filters, columns):
+	if not (filters.item_code and filters.warehouse and filters.from_date):
+		return
+
+	from erpnext.stock.stock_ledger import get_previous_sle
+	last_entry = get_previous_sle({
+		"item_code": filters.item_code,
+		"warehouse_condition": get_warehouse_condition(filters.warehouse),
+		"posting_date": filters.from_date,
+		"posting_time": "00:00:00"
+	})
+	row = {}
+	row["item_code"] = _("'Opening'")
+	for dummy, v in ((9, 'qty_after_transaction'), (11, 'valuation_rate'), (12, 'stock_value')):
+			row[v] = last_entry.get(v, 0)
+
+	return row
+
+def get_warehouse_condition(warehouse):
+	warehouse_details = frappe.db.get_value("Warehouse", warehouse, ["lft", "rgt"], as_dict=1)
+	if warehouse_details:
+		return " exists (select name from `tabWarehouse` wh \
+			where wh.lft >= %s and wh.rgt <= %s and warehouse = wh.name)"%(warehouse_details.lft,
+			warehouse_details.rgt)
+
+	return ''
+
+def get_item_group_condition(item_group):
+	item_group_details = frappe.db.get_value("Item Group", item_group, ["lft", "rgt"], as_dict=1)
+	if item_group_details:
+		return "item.item_group in (select ig.name from `tabItem Group` ig \
+			where ig.lft >= %s and ig.rgt <= %s and item.item_group = ig.name)"%(item_group_details.lft,
+			item_group_details.rgt)
+
+	return ''
