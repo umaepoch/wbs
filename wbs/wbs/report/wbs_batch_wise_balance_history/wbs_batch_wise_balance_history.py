5ca7c55e8321a4fcb0f23f759dd1ecbfc71195a0
NightsOfCoding <yashgowdaan@gmail.com>
2022-02-14 15:53:53 +0530

    WBS batch wise balance history report test1.

--- wbs/wbs/report/wbs_batch_wise_balance_history/wbs_batch_wise_balance_history.py
+++ wbs/wbs/report/wbs_batch_wise_balance_history/wbs_batch_wise_balance_history.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe import _
 from frappe.utils import flt, cint, getdate
+from wbs.wbs.doctype.wbs_storage_location.wbs_storage_location import get_entry_detail, get_id
 
 def execute(filters=None):
 	if not filters: filters = {}
@@ -14,6 +15,7 @@ def execute(filters=None):
 
 	float_precision = cint(frappe.db.get_default("float_precision")) or 3
 
+	validate_date(filters)
 	columns = get_columns(filters)
 	item_map = get_item_details(filters)
 	iwb_map = get_item_warehouse_batch_map(filters, float_precision)
@@ -23,9 +25,30 @@ def execute(filters=None):
 		for wh in sorted(iwb_map[item]):
 			for batch in sorted(iwb_map[item][wh]):
 				qty_dict = iwb_map[item][wh][batch]
-				if qty_dict.opening_qty or qty_dict.in_qty or qty_dict.out_qty or qty_dict.bal_qty:
+				if qty_dict.opening_qty or qty_dict.in_qty or qty_dict.out_qty or qty_dict.bal_qty and filters.get('wbs_settings'):
+
+					# fetch locations based on VOUCHER_NO and VOUCHER_DETAIL_NO.
+					strg_loc = {}
+					details = get_entry_detail(qty_dict.voucher_no, wh, item, qty_dict.voucher_detail_no)
+
+					if details.get('s_warehouse') == wh and item == details.get('item_code'):
+						id = get_id(details.get('source_warehouse_storage_location'))
+						if id:
+							strg_loc.update({
+								'wbs_storage_location': details.get('source_warehouse_storage_location'),
+								'wbs_id': id
+							})
+
+					if details.get('t_warehouse') == wh and item == details.get('item_code'):
+						id = get_id(details.get('target_warehouse_storage_location'))
+						if id:
+							strg_loc.update({
+								'wbs_storage_location': details.get('target_warehouse_storage_location'),
+								'wbs_id': id
+							})
+
 					data.append([item, item_map[item]["item_name"], item_map[item]["description"],
-					 	qty_dict.voucher_no, wh, batch,
+					 	strg_loc.get('wbs_storage_location'), strg_loc.get('wbs_id'),qty_dict.voucher_no, wh, batch,
 						flt(qty_dict.opening_qty, float_precision), flt(qty_dict.in_qty, float_precision),
 						flt(qty_dict.out_qty, float_precision), flt(qty_dict.bal_qty, float_precision),
 						 item_map[item]["stock_uom"]
@@ -33,11 +56,28 @@ def execute(filters=None):
 
 	return columns, data
 
+
+def validate_date(filters):
+	if filters.get('wbs_settings'):
+		actual_from_date = get_start_date(filters.get('wbs_settings'))
+		actual_to_date = get_end_date(filters.get('wbs_settings'))
+		selected_from_date = datetime.strptime(filters.get('from_date'), '%Y-%m-%d').date() if filters.get('from_date') else ''
+		selected_to_date = datetime.strptime(filters.get('to_date'), '%Y-%m-%d').date() if filters.get('to_date') else ''
+
+		if actual_from_date.get('from_date') and actual_to_date.get('to_date'):
+			if selected_from_date < actual_from_date.get('from_date') or selected_from_date > actual_to_date.get('to_date') or selected_to_date > actual_to_date.get('to_date') or selected_to_date < actual_from_date.get('from_date'):
+				frappe.throw(_('From and To date should be between WBS Settings Duration'))
+		if actual_from_date.get('from_date') and actual_to_date.get('INFINITE'):
+			if selected_from_date < actual_from_date.get('from_date'):
+				frappe.throw(_('From and To date should be between WBS Settings Duration'))
+
+	return
+
 def get_columns(filters):
 	"""return columns based on filters"""
 
 	columns = [_("Item") + ":Link/Item:100"] + [_("Item Name") + "::150"] + [_("Description") + "::150"] + \
-	[_("Voucher #") + ":Link/Stock Entry:100"] + \
+	[_("WBS Storage Location") + ":Link/WBS Storage Location:150"] +[_("WBS ID") + "::100"]+ [_("Voucher #") + ":Link/Stock Entry:100"] + \
 	[_("Warehouse") + ":Link/Warehouse:100"] + [_("Batch") + ":Link/Batch:100"] + [_("Opening Qty") + ":Float:90"] + \
 	[_("In Qty") + ":Float:80"] + [_("Out Qty") + ":Float:80"] + [_("Balance Qty") + ":Float:90"] + \
 	[_("UOM") + "::90"]
